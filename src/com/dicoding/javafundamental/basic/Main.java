cac6bc61aa6c7cad77a7d144e25f016b831364e8
Riyan Nur Rizal <riyannurrizal@gmail.com>
2019-01-12 22:53:06 +0700

    latihan struktur dasar

---
+++ src/com/dicoding/javafundamental/basic/Main.java
@@ -0,0 +1,11 @@
+package com.dicoding.javafundamental.basic;
+
+import com.dicoding.javafundamental.basic.musik.Gitar;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello world!");
+        Gitar.bunyi();
+        Gitar.senar();
+    }
+}
