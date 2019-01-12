9b6e277ee410db8a1216c2862421ffb75adcd31b
Riyan Nur Rizal <riyannurrizal@gmail.com>
2019-01-12 23:06:35 +0700

    penambahan package musik, kendaraan dan class; gitar, mobil, motor, dan kereta

--- src/com/dicoding/javafundamental/basic/Main.java
+++ src/com/dicoding/javafundamental/basic/Main.java
@@ -1,5 +1,17 @@
 package com.dicoding.javafundamental.basic;
 
+//import com.dicoding.javafundamental.basic.kendaraan.Kereta;
+//import com.dicoding.javafundamental.basic.kendaraan.Mobil;
+//import com.dicoding.javafundamental.basic.kendaraan.Motor;
+
+// Baris di atas yang di komentari bisa di sederhanakan seperti ini
+// tanda bintang setelah kendaraan itu dinamakan wildcard
+// import com.dicoding.javafundamental.basic.kendaraan.*;
+// itu tidak disarankan karena akan memperlama waktu untuk pencarian class tersebut jika banyak
+// untuk mengatur import yang optimal pilih menu CODE lalu OPTIMIZE IMPORTS
+import com.dicoding.javafundamental.basic.kendaraan.Kereta;
+import com.dicoding.javafundamental.basic.kendaraan.Mobil;
+import com.dicoding.javafundamental.basic.kendaraan.Motor;
 import com.dicoding.javafundamental.basic.musik.Gitar;
 
 public class Main {
@@ -7,5 +19,8 @@ public class Main {
         System.out.println("Hello world!");
         Gitar.bunyi();
         Gitar.senar();
+        Mobil.jumlahBan();
+        Motor.jumlahBan();
+        Kereta.jumlahBan();
     }
 }
