b5a5036d6d64fafb072b5d62a0d2c592537d1bd6
Riyan Nur Rizal <riyannurrizal@gmail.com>
2019-01-15 05:18:58 +0700

    penambahan operasi aritmatika

---
+++ src/com/dicoding/javafundamental/basic/aritmatika/OperatorAritmatika.java
@@ -0,0 +1,36 @@
+package com.dicoding.javafundamental.basic.aritmatika;
+
+public class OperatorAritmatika {
+    public static void main(String[] args) {
+        System.out.println("Operasi Penjumlahan ");
+        int hasilPenjumlahan = 8 + 5;
+        System.out.println("Hasil 8 + 5 = " + hasilPenjumlahan);
+        //System.out.println();
+
+        System.out.println("Operasi pengungarang ");
+        int hasilPengurangan = 8 - 5;
+        System.out.println("Hasil 8-5 = "+ hasilPengurangan);
+        System.out.println();
+
+        System.out.println("Operasi Perkalian ");
+        int hasilPerkalian = 5 * 5;
+        System.out.println("Hasil 5*5 = "+ hasilPerkalian);
+        System.out.println();
+
+        System.out.println("Operasi pembagian");
+        int hasilPermbagian = 20 /2 ;
+        System.out.println("Hasil 20/2 = "+ hasilPermbagian);
+        System.out.println();
+
+        System.out.println("Operasi Habis Bagi ");
+        int hasilSisa = 8 % 2;
+        System.out.println("Hasil 8 % 2 = "+ hasilSisa);
+        System.out.println();
+        int hasilSisa2 = 9 % 2;
+        System.out.println("Hasil 9 % 2 = "+ hasilSisa2);
+        System.out.println();
+
+
+
+    }
+}
